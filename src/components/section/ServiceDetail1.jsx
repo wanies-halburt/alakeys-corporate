068dfb6a339e769161aa43cd0ef49b1c4c87c3de
Justiniyke <justiniyke1995@gmail.com>
2025-02-13 01:26:09 +0100

    add to cart

--- src/components/section/ServiceDetail1.jsx
+++ src/components/section/ServiceDetail1.jsx
@@ -4,10 +4,29 @@
 import { StickyContainer } from "react-sticky";
 import useScreen from "@/hook/useScreen";
 import Image from "next/image";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useAuthStore } from "@/store/authStore";
 
-export default function ServiceDetail1({ img, title, description }) {
+export default function ServiceDetail1({ img, title, description, _id }) {
   const isMatchedScreen = useScreen(1216);
+  const { user } = useAuthStore();
 
+  const handleAddToCart = async () => {
+    if (!user) {
+      toast.error("You need to be logged in to add a product as favourite!!");
+    } else {
+      const token = localStorage.getItem("alakeys-token");
+      const payload = { productId: _id };
+      const res = await axios.post(`/api/add-cart`, payload, {
+        headers: {
+          authorization: `${token}`, // Assuming you're using a Bearer token
+        },
+      });
+      toast.success(res.data?.message || "Product added to cart");
+      console.log("res", res);
+    }
+  };
   return (
     <>
       <StickyContainer>
@@ -26,7 +45,10 @@ export default function ServiceDetail1({ img, title, description }) {
                     className=" w-full shadow rounded"
                   />
                   <div className="mt40 w-fit max-w-[140px] cursor-pointer">
-                    <button className="ud-btn btn-thm cursor-pointer">
+                    <button
+                      className="ud-btn btn-thm cursor-pointer"
+                      onClick={handleAddToCart}
+                    >
                       Add to Cart
                       <i className="fal fa-arrow-right-long" />
                     </button>
