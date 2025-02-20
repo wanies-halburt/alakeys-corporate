bc38e6f9b3f32afd08ce10422d5089e9fa56cb22
Justiniyke <justiniyke1995@gmail.com>
2025-02-20 02:29:24 +0100

    update to the profile page settings

--- src/app/(protected)/my-profile/page.js
+++ src/app/(protected)/my-profile/page.js
@@ -1,7 +1,124 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useAuthStore } from "@/store/authStore";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Profile = () => {
-  return <div className=" p-5">My Profile</div>;
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const { user } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setFullName(user.fullName);
+    setPhone(user.phone);
+    setUserName(user.userName);
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const token = localStorage.getItem("alakeys-token");
+
+    const payload = {
+      fullName,
+      userName,
+      phone,
+      oldPassword,
+      newPassword,
+    };
+    try {
+      const res = await axios.patch(`/api/update-user`, payload, {
+        headers: {
+          authorization: `${token}`, // Assuming you're using a Bearer token
+        },
+      });
+      toast.success(res.data?.message || "Profile Updated successfully");
+      console.log("order response", res);
+      setIsLoading(false);
+    } catch (err) {
+      toast.error(err?.response?.data?.message || "An error occured");
+    }
+  };
+  return (
+    <div className=" p-5">
+      <form className="form2">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="mb25">
+              <h6 className="mb15">First Name</h6>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="mb25">
+              <h6 className="mb15">User name</h6>
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col-sm-12">
+            <div className="mb25">
+              <h6 className="mb15">Phone Number</h6>
+              <input
+                className="form-control"
+                type="tel"
+                name="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col-sm-12">
+            <div className="mb25">
+              <h6 className="mb15">Old Password</h6>
+              <input
+                className="form-control"
+                type="password"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col-sm-12">
+            <div className="mb25">
+              <h6 className="mb15">New Password</h6>
+              <input
+                className="form-control"
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <button
+            disabled={isLoading}
+            onClick={handleSubmit}
+            isLoading={isLoading}
+            className="ud-btn btn-thm"
+          >
+            Update User details
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
---
+++ src/app/api/update-user/route.js
@@ -0,0 +1,73 @@
+import connectMongoDB from "@/dbConfig/mongodb";
+import { throwUserResponse } from "@/utils";
+import Client from "../register/register.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+connectMongoDB();
+
+async function updatePassword(userId, oldPassword, newPassword) {
+  const user = await Client.findById(userId);
+  const isMatch = await bcrypt.compare(oldPassword, user.password);
+  console.log("userId", userId);
+  console.log("old", oldPassword);
+  console.log("new", newPassword);
+
+  if (!isMatch) {
+    console.log("no match");
+    throw Error("Old password is incorrect");
+  }
+
+  user.password = await bcrypt.hash(newPassword, 8);
+  console.log("user pass", user.password);
+  await user.save();
+  console.log("user password", user);
+}
+
+export async function PATCH(req) {
+  const token = req.headers.get("authorization");
+  const reqBody = await req.json();
+
+  if (!token) {
+    return throwUserResponse({
+      status: 401,
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret");
+    const userId = decoded._id;
+
+    if (reqBody.newPassword) {
+      await updatePassword(userId, reqBody.oldPassword, reqBody.newPassword);
+    }
+
+    const user = await Client.findById(userId);
+    if (!user) {
+      return throwUserResponse({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+    }
+    user.userName = reqBody.userName;
+    user.fullName = reqBody.fullName;
+    user.phone = reqBody.phone;
+    await user.save();
+    return throwUserResponse({
+      status: 200,
+      success: true,
+      message: "User details successfully updated",
+      data: user,
+    });
+  } catch (error) {
+    return throwUserResponse({
+      status: 500,
+      success: false,
+      message: error?.message,
+      errorStack: error,
+    });
+  }
+}
